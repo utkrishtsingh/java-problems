4aebb855504c032948b099745619faf685691152
Utkrisht Singh <utkrisht.singh@socure.com>
2023-06-02 13:17:28 +0530

    Adding Bubble Sort and Selection Sort Implementations

--- array/ReverseArray.java
+++ array/ReverseArray.java
@@ -1,21 +1,21 @@
 public class ReverseArray {
-    public static void main(String ...args) {
+    public static void main(String... args) {
         int[] arr = new int[]{1, 2, 3, 4, 5};
-	arr = reverseArray(0, arr.length, arr);
-	for(int number: arr) {
-	    System.out.println(number);
-	}
+        arr = reverseArray(0, arr.length, arr);
+        for (int number : arr) {
+            System.out.println(number);
+        }
     }
 
     private static int[] reverseArray(int beginIndex, int endIndex, int[] arr) {
         int l = beginIndex, r = endIndex - 1;
-	while (l < r) {
-	    int temp = arr[l];
-	    arr[l] = arr[r];
-	    arr[r] = temp;
-	    l++;
-	    r--;
-	}
+        while (l < r) {
+            int temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
         return arr;
     }
 }
